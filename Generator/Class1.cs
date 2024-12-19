00101cb51e75a52299f5817ad9a360cd82d1dc12
Nickky White <nickkywhite05@gmail.com>
2024-12-19 18:26:39 +0200

    Библиотека генератор

---
+++ Generator/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Generator;
+
+
+namespace Generator
+{
+    public class RandNum
+    {
+        private Random _random;
+
+        public RandNum()
+        {
+            _random = new Random();
+        }
+
+        public int GetRandInt(int min, int max)
+        {
+            return _random.Next(min, max);
+        }
+
+        public double GetRandDbl(double min, double max)
+        {
+            return _random.NextDouble() * (max - min) + min;
+        }
+    }
+
+    public class RandStr
+    {
+        private Random _random;
+        private const string Chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        public RandStr()
+        {
+            _random = new Random();
+        }
+
+        public string GetRandStr(int length)
+        {
+            char[] result = new char[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                result[i] = Chars[_random.Next(Chars.Length)];
+            }
+
+            return new string(result);
+        }
+    }
+}
---
+++ Generator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Security.Cryptography;
+using Generator;
+
+namespace Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RandNum numGen = new RandNum();
+            Console.WriteLine("Случайное целое число (1-100): " + numGen.GetRandInt(1, 100));
+            Console.WriteLine("Случайное дабл число (0-1): " + numGen.GetRandDbl(0, 1));
+            RandStr strGen = new RandStr();
+            Console.WriteLine("Случайная строка с заданной длиной: " + strGen.GetRandStr(10));
+        }
+    }
+}
